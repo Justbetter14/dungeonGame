7db7c586dc3f852a8d95b0bb7262bf1da2068ca9
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2025-11-25 11:35:17 -0500

    restart

--- dungeonGame/dungeon Game/Player/character.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-class_name Character
-
-@export var health: int
-@export var icon: Texture
-@export var speed: int
-@export var dashCharges: int
--- dungeonGame/dungeon Game/Weapon/attack.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-
-class_name Attack
-
-@export var damage: int
-@export var knockbackForce: float
-
-func knockback(knockbackDirection: float):
-	pass
